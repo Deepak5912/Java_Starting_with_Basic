16cddbcb4a08427c6f62b2955c6d0a3617eb601c
D55p1 <deepak591kr@gmail.com>
2025-12-06 11:59:24 +0530

    print the no. from 1 to 100 without using any no.

---
+++ src/Interface.java
@@ -0,0 +1,12 @@
+
+
+/// print the no. from 1 to 100 without using any no.
+public class Interface {
+    
+    public static void main(String [] args){
+        for (int i = 'A'-'@'; i < 'e' ; i++) {
+            System.out.println(i);
+            
+        }}
+}
+    
