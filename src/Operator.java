31bc0fa6671905932e08b54905f32e7298924c6e
D55p1 <deepak591kr@gmail.com>
2025-12-06 11:00:25 +0530

    play with operator precedence

---
+++ src/Operator.java
@@ -0,0 +1,16 @@
+//public class Operator {
+//    public static void main(String[] args) {
+//        int a = 5;
+//        System.out.println(a++ + ++a);
+//    }
+//}
+// their output wil be  12 = 6+ 6
+
+
+public class Operator {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a + ++a);
+    }
+}
+// he will the give the output 11/
