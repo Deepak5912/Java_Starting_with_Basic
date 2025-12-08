a38247adf1dc6ba760660987291bd2a2973b2c6d
Deepak5912 <deepak591kr@gmail.com>
2025-12-08 23:56:39 +0530

    while loop

---
+++ src/Do_While.java
@@ -0,0 +1,10 @@
+public class Do_While {
+    public static void main(String[] args) {
+        int n = 12;
+        while(n<15){
+            System.out.println("radha radha ");
+            n++;
+        }
+        System.out.println("do whole lopp comlitie " + "reveson completed");
+    }
+}
