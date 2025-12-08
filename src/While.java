3781e70039fb6f9445bdd7513bda460a6a47386e
Deepak5912 <deepak591kr@gmail.com>
2025-12-08 23:51:01 +0530

    while loop

---
+++ src/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int n = 50;
+        while (n < 100) {
+            System.out.println("Subhan Allah + Aliya ");
+            n++;
+        }
+    }
+}
