ed178e2b6e88ee9aa381b063cd56767ebf681389
Deepak5912 <deepak591kr@gmail.com>
2025-12-09 23:21:28 +0530

    Counting String to Array

---
+++ src/Char.java
@@ -0,0 +1,9 @@
+public class Char {
+    public static void main(String[] args) {
+        String str = "HELLO WORLD";
+        char [] Arr1 = str.toCharArray();
+        for (char c : Arr1) {
+            System.out.println(c);
+        }
+    }
+}
