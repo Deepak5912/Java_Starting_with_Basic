58c349e156642287b48e0c59f8271ee2d780af3f
Deepak5912 <deepak591kr@gmail.com>
2025-12-11 23:28:35 +0530

    print the arry

---
+++ src/Reverse.java
@@ -0,0 +1,10 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int [] arr = {1,4,5,6,7,3};
+        int start = 0;
+
+        for(int j = 0; j < arr.length; j++){
+            System.out.print(arr[j]+" ");
+        }
+    }
+}
