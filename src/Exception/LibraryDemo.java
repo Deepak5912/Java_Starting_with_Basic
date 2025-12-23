46e89d5dd0a4afdaa8e602cf0a5e19708433123e
Deepak5912 <deepak591kr@gmail.com>
2025-12-23 23:01:54 +0530

    only request a book with using of error handling with the using of throws keyword

--- src/Exception/LibraryDemo.java
+++ src/Exception/LibraryDemo.java
@@ -1,21 +1,16 @@
 package Exception;
 class Library{
     int avialbaleBooks = 3;
-    public void borrowBook(int bookrequestedCount){
-        try{
+    public void borrowBook(int bookrequestedCount) throws Exception {
+
 //            int [] books = {101,102,103};
 //            System.out.println("book requested :"+ books[bookrequested]);
             if(bookrequestedCount > avialbaleBooks){
-               throw new ArrayIndexOutOfBoundsException("Requested book count is more than available books");
+               throw new Exception("Requested book count is more than available books");
+            }if(bookrequestedCount < 0){
+                throw new Exception("Requested book count cannot be negative");
             }
 
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Exception Occured :  so thay why books are not availabe");
-        }catch (Exception e){
-            System.out.println("Null Pointer Exception occured ");
-        }finally {
-            System.out.println("Available books are :"+ avialbaleBooks);
-        }
 
     }
 
@@ -23,10 +18,51 @@ class Library{
 public class LibraryDemo {
     public static void main(String[] args) {
         Library library = new Library();
-        library.borrowBook(5);
+        try {
+            library.borrowBook(-8);
+        } catch (Exception e) {
+            System.out.println("exception occured :"+ e.getMessage());
+        }
 
     }
 }
+
+// ###################### using of throw keyword #########################
+
+//public void borrowBook(int bookrequestedCount){
+//    try{
+////            int [] books = {101,102,103};
+////            System.out.println("book requested :"+ books[bookrequested]);
+//        if(bookrequestedCount > avialbaleBooks){
+//            throw new ArrayIndexOutOfBoundsException("Requested book count is more than available books");
+//        }
+//
+//    }catch (ArrayIndexOutOfBoundsException e){
+//        System.out.println("Exception Occured :  so thay why books are not availabe");
+//    }catch (Exception e){
+//        System.out.println("Null Pointer Exception occured ");
+//    }finally {
+//        System.out.println("Available books are :"+ avialbaleBooks);
+//    }
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
