be896c32736d78705cf59124b6bfa11516496832
Deepak5912 <deepak591kr@gmail.com>
2025-12-23 22:43:27 +0530

    only request a book with using of error handling

--- src/Exception/LibraryDemo.java
+++ src/Exception/LibraryDemo.java
@@ -2,15 +2,23 @@ package Exception;
 class Library{
     int avialbaleBooks = 3;
     public void borrowBook(int bookrequested){
-        int [] books = {101,102,103};
-        System.out.println("book requested :"+ books[bookrequested]);
+        try{
+            int [] books = {101,102,103};
+            System.out.println("book requested :"+ books[bookrequested]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Exception Occured :  so thay why books are not availabe");
+        }catch (NullPointerException e){
+            System.out.println("Null Pointer Exception occured ");
+        }finally {
+            System.out.println("Available books are :"+ avialbaleBooks);
+        }
 
     }
 }
 public class LibraryDemo {
     public static void main(String[] args) {
         Library library = new Library();
-        library.borrowBook(2);
+        library.borrowBook(6);
 
     }
 }
