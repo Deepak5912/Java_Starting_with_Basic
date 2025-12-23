e5fbcf0af8a335966f2ca7fcc52a67f189027290
Deepak5912 <deepak591kr@gmail.com>
2025-12-23 22:52:02 +0530

    only request a book with using of error handling with the using of throw keyword

--- src/Exception/LibraryDemo.java
+++ src/Exception/LibraryDemo.java
@@ -1,24 +1,45 @@
 package Exception;
 class Library{
     int avialbaleBooks = 3;
-    public void borrowBook(int bookrequested){
+    public void borrowBook(int bookrequestedCount){
         try{
-            int [] books = {101,102,103};
-            System.out.println("book requested :"+ books[bookrequested]);
+//            int [] books = {101,102,103};
+//            System.out.println("book requested :"+ books[bookrequested]);
+            if(bookrequestedCount > avialbaleBooks){
+               throw new ArrayIndexOutOfBoundsException("Requested book count is more than available books");
+            }
+
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Exception Occured :  so thay why books are not availabe");
-        }catch (NullPointerException e){
+        }catch (Exception e){
             System.out.println("Null Pointer Exception occured ");
         }finally {
             System.out.println("Available books are :"+ avialbaleBooks);
         }
 
     }
+
 }
 public class LibraryDemo {
     public static void main(String[] args) {
         Library library = new Library();
-        library.borrowBook(6);
+        library.borrowBook(5);
 
     }
 }
+
+
+
+//public void borrowBook(int bookrequested){
+//    try{
+//        int [] books = {101,102,103};
+//        System.out.println("book requested :"+ books[bookrequested]);
+//    }catch (ArrayIndexOutOfBoundsException e){
+//        System.out.println("Exception Occured :  so thay why books are not availabe");
+//    }catch (NullPointerException e){
+//        System.out.println("Null Pointer Exception occured ");
+//    }finally {
+//        System.out.println("Available books are :"+ avialbaleBooks);
+//    }
+//
+//}
