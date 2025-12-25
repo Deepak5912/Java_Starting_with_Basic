f3f6b2f60254a3ebbd34ab8daa35c3cf7223d770
Deepak5912 <deepak591kr@gmail.com>
2025-12-25 09:59:21 +0530

    Bifunction

---
+++ src/Day6/BiFunctionDemo.java
@@ -0,0 +1,16 @@
+package Day6;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionDemo {
+    public static BiFunction<Integer, Integer, Integer> add = Integer::sum;
+    public static BiFunction<Integer, Integer, Integer> sub = (a, b) -> a - b;
+
+
+    public static void main(String[] args) {
+        System.out.println("the result of addition is :" + add.apply(5, 10) );
+        System.out.println("the result of subtraction is :" + sub.apply(10, 5) );
+
+        }
+    }
+
