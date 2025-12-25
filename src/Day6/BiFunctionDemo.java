840eeb654710b17ebb03b4f9954d829e9492107c
Deepak5912 <deepak591kr@gmail.com>
2025-12-25 10:05:32 +0530

    Add multiplication method to BiFunctionDemo

--- src/Day6/BiFunctionDemo.java
+++ src/Day6/BiFunctionDemo.java
@@ -5,12 +5,15 @@ import java.util.function.BiFunction;
 public class BiFunctionDemo {
     public static BiFunction<Integer, Integer, Integer> add = Integer::sum;
     public static BiFunction<Integer, Integer, Integer> sub = (a, b) -> a - b;
+    public static Integer multiply(int a, int b) {
+        return a * b;
+    }
 
 
     public static void main(String[] args) {
         System.out.println("the result of addition is :" + add.apply(5, 10) );
         System.out.println("the result of subtraction is :" + sub.apply(10, 5) );
-
+        System.out.println("the result of multiplication is :" + multiply(5, 10) );
         }
     }
 
