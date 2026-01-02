202cca845096201a8e0694f222ccce925f02f419
Deepak5912 <deepak591kr@gmail.com>
2026-01-02 18:25:34 +0530

    Add Str class to demonstrate string manipulation methods

---
+++ src/Str.java
@@ -0,0 +1,27 @@
+public class Str {
+    public static void main(String[] args) {
+        String str = "may i come in peace";
+        System.out.println(str);
+        String str1 = new String("may i Come in peace");
+        System.out.println(str1);
+        if (str.equals(str1)) {
+            System.out.println("Both are equal");
+        } else {
+            System.out.println("Both are not equal");
+        }
+        System.out.println(str.compareTo(str1));
+        System.out.println(str.toUpperCase() + " is equal to " + str1.toUpperCase());
+        System.out.println(str.length());
+        System.out.println(str.charAt(5));
+        System.out.println(str.indexOf('i'));
+        System.out.println(str1.indexOf('c'));
+        System.out.println(str.substring(4,13));
+        if(str.endsWith("peace")) {
+            System.out.println("String ends with peace");
+        } else {
+            System.out.println("String does not end with peace");
+        }
+        System.out.println(str.compareToIgnoreCase(str1));
+        System.out.println(str.split("come")[2]);
+    }
+}
