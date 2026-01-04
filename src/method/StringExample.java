085764e822c586fe4de516038ffa3d3204eee96d
Deepak5912 <deepak591kr@gmail.com>
2026-01-04 23:59:10 +0530

    Add Greeting and MethodInJava classes for Day 9 of Java programming

---
+++ src/method/StringExample.java
@@ -0,0 +1,14 @@
+package method;
+
+public class StringExample {
+    static void main() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name());
+        }
+
+    }
+    static  String name(){
+        return "Hello, World! retun name ";
+        //System.out.println("---");   -- . its vive a unrachbale statement
+    }
+}
