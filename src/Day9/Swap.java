9a102acc5c2a91ef0b3f6db5da334ee679e944da
Deepak5912 <deepak591kr@gmail.com>
2026-01-05 18:10:09 +0530

    and enhance Swap class with a swap method

--- src/Day9/Swap.java
+++ src/Day9/Swap.java
@@ -6,9 +6,20 @@ public class Swap {
         int b = 20;
         System.out.println("Before Swap: a = " + a + ", b = " + b);
         // Swapping logic
+
+
         int temp = a;
         a = b;
         b = temp;
         System.out.println("After Swap: a = " + a + ", b = " + b);
+        swap(50,60);
+
+    }
+    static void  swap(int c, int d) {
+        int temp = c;
+        c = d;
+        d = temp;
+        System.out.println("After Swap Method Call: a = " + c + ", b = " + d);
+
     }
 }
