50764fb953a0f54002bca212d10de44524c2ca33
Deepak5912 <deepak591kr@gmail.com>
2026-01-15 22:28:49 +0530

    Add chall4 and chall5 classes for basic arithmetic operations

---
+++ src/basic_Revision/chall4.java
@@ -0,0 +1,9 @@
+package basic_Revision;
+
+public class chall4 {
+    static void main() {
+        float a = 20f;
+        float b = 30f;
+        System.out.println("the product  of two floating no. are : "  + a * b);
+    }
+}
---
+++ src/basic_Revision/chall5.java
@@ -0,0 +1,12 @@
+package basic_Revision;
+
+public class chall5 {
+    static void main() {
+        int i = 1;
+        int n = 10;
+        while (i <= 10) {
+            System.out.println("the multiplicattion of table : " +  n*i);
+            i++;
+        }
+    }
+}
