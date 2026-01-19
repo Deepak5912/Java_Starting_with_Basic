8620d790b8acb374e1190a124653b3a8f7435065
Deepak5912 <deepak591kr@gmail.com>
2026-01-19 09:38:31 +0530

    Add chall9 class for counting occurrences of an element in an array

---
+++ src/basic_Revision/chall9.java
@@ -0,0 +1,22 @@
+package basic_Revision;
+
+public class chall9 {
+    static void main() {
+        int [] arr1 = {1,5,6,1,5};
+        int count = 0;
+        int element = 1;
+        for(int i = 0; i<arr1.length; i++){
+            System.out.println(arr1[i]);
+            if (arr1[i]==element){
+                count++;
+            }
+
+
+
+            }System.out.println("the count is : " + count);
+
+        }
+
+
+    }
+
