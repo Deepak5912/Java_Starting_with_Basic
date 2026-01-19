90a3de7edd92f759bea3b20c02079396f22e9ac7
Deepak5912 <deepak591kr@gmail.com>
2026-01-19 10:22:11 +0530

    Add chall12 class for reversing and printing an array

---
+++ src/basic_Revision/chall12.java
@@ -0,0 +1,12 @@
+package basic_Revision;
+
+public class chall12 {
+    static void main() {
+        int [] arr = {2,3,4,5,6,7,8,9,10};
+        int len = arr.length;
+        for (int i = len; i>0; i--){
+
+            System.out.println(arr[i-1]);
+        }
+    }
+}
